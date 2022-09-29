1fe8144192d0c523f71f76ee53274b6477188e92
loftol <tkfkddl59323@naver.com>
2022-09-30 07:33:51 +0900

    add data and generalization

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
 import './App.css';
 import React, {useEffect} from 'react';
-import data from './tmpdata/mock';
+import datas from './tmpdata/mock';
 
 const {kakao} = window;
 
 function App() {
   const windowHeight = window.innerHeight;
-  const markerPosition = data.sogangUniv.lating;
-  const marker = new kakao.maps.Marker({
-    position: markerPosition,
-    clickable: true,
-  });
-  const iwContent = `<div style = "padding:5px;">sogangUniv</div>`;
-  const infoWindow = new kakao.maps.InfoWindow({
-    content: iwContent,
-    removable: true,
-  });
 
   useEffect(() => {
     const container = document.getElementById('map');
+
     const option = {
-      center: data.sogangUniv.lating,
+      center: datas[0].lating,
     };
+
     const map = new kakao.maps.Map(container, option);
 
-    marker.setMap(map);
+    datas.forEach(function (data) {
+      const markerPosition = data.lating;
 
-    kakao.maps.event.addListener(marker, 'click', function () {
-      infoWindow.open(map, marker);
-      if (window.ReactNativeWebView) {
-        window.ReactNativeWebView.postMessage(
-          JSON.stringify({id: data.sogangUniv.id}),
-        );
-      }
+      const marker = new kakao.maps.Marker({
+        position: markerPosition,
+        clickable: true,
+      });
+
+      marker.setMap(map);
+
+      const iwContent = `<div style = "padding:5px;">${data.name}</div>`;
+      const infoWindow = new kakao.maps.InfoWindow({
+        content: iwContent,
+        removable: true,
+      });
+      kakao.maps.event.addListener(marker, 'click', function () {
+        infoWindow.open(map, marker);
+        if (window.ReactNativeWebView) {
+          window.ReactNativeWebView.postMessage(JSON.stringify({id: data.id}));
+        }
+      });
     });
   }, []);
 
--- src/tmpdata/mock.js
+++ src/tmpdata/mock.js
@@ -1,10 +1,23 @@
 const {kakao} = window;
 
-const datas = {
-  sogangUniv: {
-    id: '1',
-    lating: new kakao.maps.LatLng(37.551629, 126.937933),
-  },
+const sogangUniv = {
+  id: '1',
+  name: 'sogangUniv',
+  lating: new kakao.maps.LatLng(37.551629, 126.937933),
 };
+
+const WooZip = {
+  id: '2',
+  name: 'WooZip',
+  lating: new kakao.maps.LatLng(37.55157, 126.936881),
+};
+
+const HoZip = {
+  id: '3',
+  name: 'HoZip',
+  lating: new kakao.maps.LatLng(37.548972, 126.937359),
+};
+
+const datas = [sogangUniv, WooZip, HoZip];
 
 export default datas;
